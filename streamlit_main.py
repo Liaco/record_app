197db7bf50e6f76416d48a3f34ab547e43f79fb1
Ke Lee <45429398+Liaco@users.noreply.github.com>
2023-08-03 19:00:23 +0800

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+with st.sidebar:
+    st.latex(r'''
+             A = \begin{bmatrix}
+    a & b\\
+    c & d
+    \end{bmatrix}''')
+
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
