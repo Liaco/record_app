9f1eee3ba2374f86cbecf0a89bbee07e14914bbd
Ke Lee <45429398+Liaco@users.noreply.github.com>
2023-08-03 19:00:46 +0800

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -10,4 +10,3 @@ with st.sidebar:
 st.markdown('Streamlit is **_really_ cool**.')
 st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
 st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
-st.snow()
